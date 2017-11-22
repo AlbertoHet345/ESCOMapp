855f0088a754054425cb063228252bc7bff9d017
Alberto García Paul <go_ku_7@hotmail.com>
2017-11-21 18:45:02 -0600

    Initial Commit

---
+++ ESCOMapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ESCOMapp
+//
+//  Created by Alberto Garcia on 21/11/17.
+//  Copyright © 2017 Alberto Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
